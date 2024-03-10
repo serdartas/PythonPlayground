38b68130721dfe7ada6bd36fc2f68eef593c80e7
Serdar Tas <i.serdar.tas@gmail.com>
2024-03-10 12:53:37 +0100

    cast

---
+++ 03-TypeCasting.py
@@ -0,0 +1,12 @@
+# Cast to various types
+myStringVar = str(123)
+myIntVar = int("123")
+myFloatVar = float("12.34")
+
+# to find unicode charachter code, use ord function
+print(ord('a'), ord('b'), ord('c'))
+
+# to get the hexadecimal representation of an integer, use hex function
+print(hex(1))
+
+print(oct(97))
